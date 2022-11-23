febbc4944a8fe6d120e7c55c46178eec608c1340
Heinrich Lizato <78667401+hlizato28@users.noreply.github.com>
2022-11-23 10:52:31 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+  
+error_reporting(1);
+$conn = new mysqli("sql7.freesqldatabase.com","sql7580019","hchFy4RUdP","sql7580019");
+
+?>
+
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+<footer id="contactus" style="background-color: #1f1f1f;"  >
+	
+   
+    <div class="container row" style="width:100%">
+	<div class="col-md-4 col-md-offset-2">
+	
+	<p style="color: aqua" class="footer-links">
+			<a style="color: aqua" href="#intro">Home</a>  
+		</p>
+            <h3 style="color: aqua"><a href="https://www.getsetproject.com"> Developed by <h3 style="font-family:audiowide;display:inline">Gruppe 3</h3> </a></h3>
+		
+          
+		
+		
+    </div>
+    
+    
+
+
+    
+  
+</div>
+</div>
+    
+    </div>
+	
+	
+
+	
+</footer>	
